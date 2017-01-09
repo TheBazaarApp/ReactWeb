e21c1f2fbcd9c088fc1ad5dd9f017f9d0e4a1c35
AliengirlLiv <oliviagracewatkins@gmail.com>
2017-01-08 17:14:55 -0800

    Added filtering, childChanged, childRemoved on feed

--- src/js/Feed.js
+++ src/js/Feed.js
@@ -56,7 +56,7 @@ export class Feed extends Component {
 		if (this.state.filterBy === "All Albums") {
 			itemsToShow = this.state.albums.map(
 				(album) => {
-					return( <Album key={album.albumID} album={album} />);
+					return( <Album key={album.albumID} album={album} activeIndex={0}/>);
 				})
 		} else {
 			itemsToShow = this.state.albums.map(
@@ -115,79 +115,102 @@ export class Feed extends Component {
 		for (const college of this.state.tradingList) {
 			const feedRef = firebase.database().ref().child(college + "/albums");
 			this.createChildAddedListener(feedRef, college);
-			//TODO: Add child changed listener here
-			//TODO: Add child removed listener here
+			this.createChildChangedListener(feedRef, college);
+			this.createChildRemovedListener(feedRef);
 		} 	
 	}
 
-
+	//TODO: Maybe hide image until it shows up
 	createChildAddedListener(feedRef, college) {
 		//const improveFeedRef = feedRef.queryOrderedByChild("albumDetails/timestamp");
-		feedRef.on('child_added', function(snapshot) { //child_added
-			if (snapshot) {
-				if (snapshot.child('unsoldItems') != null) {
-					//Album details
-					//TODO: Do we need any of the indexes?
-					//TODO: Make sure we handle null values sensibly
-					var newAlbum = {
-						unsoldItems: [],
-						albumID: snapshot.key,
-						albumName: snapshot.child('albumDetails/albumName').val(),
-						sellerID: snapshot.child('albumDetails/sellerID').val(),
-						sellerName: snapshot.child('albumDetails/sellerName').val(),
-						sellerCollege: college,
-						locationLat: snapshot.child('albumDetails/locationLat').val(), //TODO: Coult be problematic if these don't exist
-						locationLong: snapshot.child('albumDetails/sellerName').val(),
-						location: snapshot.child('albumDetails/location').val(),
-						timestamp: snapshot.child('albumDetails/timestamp').val()
-					};
-					//Add Items to the album
-					var itemList = snapshot.child('unsoldItems');
-					itemList.forEach(function(itemInfo) {
-						var newItem = {
-							imageKey: itemInfo.key,
-							description: itemInfo.child('description').val(),
-							tag: itemInfo.child('tag').val(),
-							name: itemInfo.child('name').val(),
-							price: itemInfo.child('price').val(),
-							timestamp: itemInfo.child('timestamp').val(),
-							picture: 'cheetah.jpg', //TODO: take this out
-							hasPic: true //Default, ISOs may be exceptions to this
-						}
-
-						//Some ISOs don't have pics
-						//TODO: Only have to do this once
-						newAlbum.isISO = (newItem.tag === "In Search Of");
-						if (newAlbum.isISO) {
-							if (itemInfo.child('hasPic').val()) {
-								newItem.hasPic = false;
-							}
-						}
-						
-						newAlbum.unsoldItems.push(newItem);
-						if (newItem.hasPic) {
-							this.getImage(newItem, newAlbum); //TODO: Implement this
-						}
-						
-					}.bind(this));
-					this.insertAlbum(newAlbum);
-					//TODO: Maybe have an "All Items" thing too
-				}
-			}
+		feedRef.on('child_added', function(snapshot) {
+			let album = this.createAlbum(snapshot, college);
+			this.insertAlbum(album);
 		}.bind(this));
 	}
 
 
+
+	createChildChangedListener(feedRef, college) {
+		feedRef.on('child_changed', function(snapshot) {
+			let album = this.createAlbum(snapshot, college);
+			this.updateAlbum(album);
+		}.bind(this));
+	}
+
+
+
+	createAlbum(snapshot, college) { //TODO: For the moment, we're not counting to see if the album needs to be deleted
+		//Album details
+		var newAlbum = {
+			unsoldItems: [],
+			albumID: snapshot.key,
+			albumName: snapshot.child('albumDetails/albumName').val(),
+			sellerID: snapshot.child('albumDetails/sellerID').val(),
+			sellerName: snapshot.child('albumDetails/sellerName').val(),
+			sellerCollege: college,
+			locationLat: snapshot.child('albumDetails/locationLat').val(), //TODO: Coult be problematic if these don't exist
+			locationLong: snapshot.child('albumDetails/sellerName').val(),
+			location: snapshot.child('albumDetails/location').val(),
+			timestamp: snapshot.child('albumDetails/timestamp').val()
+		};
+		//Add Items to the album
+		var itemList = snapshot.child('unsoldItems');
+		itemList.forEach(function(itemInfo) {
+			var newItem = {
+				imageKey: itemInfo.key,
+				description: itemInfo.child('description').val(),
+				tag: itemInfo.child('tag').val(),
+				name: itemInfo.child('name').val(),
+				price: itemInfo.child('price').val(),
+				picture: 'cheetah.jpg', //TODO: take this out
+				hasPic: true //Default, ISOs may be exceptions to this
+			}
+
+			//Some ISOs don't have pics
+			//TODO: Only have to do this once
+			newAlbum.isISO = (newItem.tag === "In Search Of");
+			if (newAlbum.isISO) {
+				if (itemInfo.child('hasPic').val()) {
+					newItem.hasPic = false;
+				}
+			}
+			
+			newAlbum.unsoldItems.push(newItem);
+			if (newItem.hasPic) {
+				this.getImage(newItem, newAlbum); //TODO: Would be more efficient if we only called this when needed
+			}
+		}.bind(this));
+		return(newAlbum);
+	}
+
+
+	createChildRemovedListener(feedRef) {
+		feedRef.on('child_removed', function(snapshot) {
+			const deletedAlbumID = snapshot.key;
+			let albums = this.state.albums;
+			for (let i = albums.length - 1; i >= 0; i--) {
+				if (albums[i].albumID === deletedAlbumID) {
+					albums.splice(i, 1);
+				}
+			}
+			this.updateState(albums);
+		}.bind(this));
+	}
+
+
+	updateState(albums) {
+		this.setState({
+			albums: albums
+		});
+	}
+
+
 	getImage(item, album) {
-		//const storageRef = FIRStorage.storage().referenceForURL("gs://bubbleu-app.appspot.com");
 		const imageRef = firebase.storage().ref().child(album.sellerCollege + '/user/' + album.sellerID + '/images/' + item.imageKey);
 		imageRef.getDownloadURL().then(function(url) {
-			//Find item
-			// Update its' state
-			// TODO: Is this passed by val or by reference?
 			item.picture = url;
 			this.updateAlbum(album);
-			//this.state.pic = 
 		}.bind(this)).catch(function(error) {
 			///console.log("didn't get pic :(     " + error);
 			//TODO: Later on, make it repeat a few times if it isn't loading.
@@ -196,7 +219,6 @@ export class Feed extends Component {
 
 
 	updateAlbum(changedAlbum) {
-
 		var albums = this.state.albums;
 		var index = 0;
 		for (var album of albums) {
@@ -206,9 +228,7 @@ export class Feed extends Component {
 			}
 			index++;
 		}
-		this.setState({
-			albums: albums
-		});
+		this.updateState(albums);
 	}
 
 	insertAlbum(newAlbum) {
@@ -216,8 +236,7 @@ export class Feed extends Component {
 		var albums = this.state.albums;
 		for (var i = 0; i < albums.length; i++) {
 			const currAlbum = albums[i];
-			//TODO: should timestamp be an individual or album-level property?
-			if (currAlbum.unsoldItems[0].timestamp > newAlbum.unsoldItems[0].timestamp) {
+			if (currAlbum.timestamp > newAlbum.timestamp) {
 				albums.splice(i, 0, newAlbum);
 				inserted = true;
 				break;
@@ -226,16 +245,11 @@ export class Feed extends Component {
 		if (!inserted) {
 			albums.push(newAlbum);
 		}
-		this.setState({
-			albums: albums
-		});
+		this.updateState(albums);
 	}
 
 
 }
-
-//
-
 
 class Album extends Component {
 	render() {
@@ -247,12 +261,13 @@ class Album extends Component {
 							return ( 
 								<Carousel.Item key={item.imageKey}>
 									<Image className="small" src={item.picture} />
+									<PriceSticker price={item.price} />
 								</Carousel.Item>
 							);
 						})}
 					</Carousel>
 				</Button>
-				<PriceSticker price={this.props.price} />
+				
 			</div>
 		)
 	}
@@ -267,8 +282,9 @@ class Item extends Component {
 			<div>
 				<Button>
 					<Image  className="small" src={this.props.item.picture} thumbnail />
+					<PriceSticker className="ugh" price={"$" + this.props.item.price} />
 				</Button>
-				<PriceSticker price={"$" + this.props.item.price} />
+				
 				{/*TODO: format price better*/}
 			</div>
 		)
